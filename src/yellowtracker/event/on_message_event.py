89f205f497bfc98eb30d133b2a1e21e1c0a6cd65
victorgawk <victor.gawk@gmail.com>
2022-11-12 10:14:03 -0300

    Added command to check track channel status
    - Added command to check track channel status (bot owner only).

--- src/yellowtracker/event/on_message_event.py
+++ src/yellowtracker/event/on_message_event.py
@@ -2,7 +2,10 @@ import logging
 import discord
 import time
 from yellowtracker.domain.bot import Bot
+from yellowtracker.domain.track_type import TrackType
+from yellowtracker.service.channel_service import ChannelService
 from yellowtracker.util.coroutine_util import CoroutineUtil
+from yellowtracker.util.track_util import TrackUtil
 
 log = logging.getLogger(__name__)
 
@@ -10,7 +13,7 @@ class OnMessageEvent:
 
     @staticmethod
     async def on_message(bot: Bot, tree: discord.app_commands.CommandTree, message: discord.Message):
-        if not OnMessageEvent.validate(bot, message):
+        if not OnMessageEvent.validate_message(bot, message):
             return
         args = message.content.split(" ", 1)
         if args[0] == "!guilds":
@@ -21,30 +24,25 @@ class OnMessageEvent:
             await OnMessageEvent.notify(bot, message.channel, args[1])
         if args[0] == "!sync":
             await OnMessageEvent.sync(bot, tree, message.channel)
+        if args[0] == "!channels":
+            await OnMessageEvent.channels(bot, message.channel)
 
     @staticmethod
-    async def guilds(bot: Bot, channel: discord.abc.Messageable):
-        page_size = 20
-        guilds = ""
-        offset = 1
-        count = 0
+    async def guilds(bot: Bot, dm_channel: discord.abc.Messageable):
+        lines = []
         for guild in bot.guilds:
             owner = None
             if guild.owner_id is not None:
                 owner = await CoroutineUtil.run(guild.fetch_member(guild.owner_id))
-            guilds += f"{guild.name} | {owner} | {guild.id}\n"
-            count += 1
-            if count == page_size:
-                await OnMessageEvent.send_guilds_partial_msg(bot, channel, offset, count, guilds)
-                guilds = ""
-                offset = offset + count
-                count = 0
-        if count > 0:
-            await OnMessageEvent.send_guilds_partial_msg(bot, channel, offset, count, guilds)
+            lines.append(f"{guild.name} | {owner} | {guild.id}")
+        title = "Guilds"
+        header = "Name | Owner | ID"
+        await OnMessageEvent.send_msg_paginated(bot, dm_channel, title, header, lines)
 
     @staticmethod
-    async def notify(bot: Bot, channel: discord.abc.Messageable, user_msg: str):
+    async def notify(bot: Bot, dm_channel: discord.abc.Messageable, user_msg: str):
         users_id = set()
+        count = 0
         for guild in bot.guilds:
             users_id.add(guild.owner_id)
         for user_id in users_id:
@@ -54,27 +52,86 @@ class OnMessageEvent:
                 msg += "\n\n"
                 msg += user_msg
                 await CoroutineUtil.run(user.send(msg))
+                count += 1
+        await CoroutineUtil.run(dm_channel.send(f"Notification sent to {count} users."))
 
     @staticmethod
-    async def sync(bot: Bot, tree: discord.app_commands.CommandTree, channel: discord.abc.Messageable):
+    async def sync(bot: Bot, tree: discord.app_commands.CommandTree, dm_channel: discord.abc.Messageable):
         ts = time.time()
-        await CoroutineUtil.run(channel.send(f"Begin sync slash commands with {len(bot.guilds)} guilds."))
+        await CoroutineUtil.run(dm_channel.send(f"Begin sync slash commands with {len(bot.guilds)} guilds."))
         for guild in bot.guilds:
             tree.copy_global_to(guild=guild)
             await CoroutineUtil.run(tree.sync(guild=guild))
-        await CoroutineUtil.run(channel.send(f"Slash commands synced with {len(bot.guilds)} guilds in {int(time.time() - ts)} secs."))
+        await CoroutineUtil.run(dm_channel.send(f"Slash commands synced with {len(bot.guilds)} guilds in {int(time.time() - ts)} secs."))
 
     @staticmethod
-    async def send_guilds_partial_msg(bot: Bot, channel: discord.abc.Messageable, offset: int, count: int, guilds: str):
-        embed = discord.Embed()
-        embed.title = "Guilds"
-        embed.colour = discord.Colour.gold()
-        embed.add_field(name="Name | Owner | ID", value=guilds, inline=False)
-        embed.set_footer(text=f"{offset} to {offset + count - 1} (Total: {len(bot.guilds)})")
-        await CoroutineUtil.run(channel.send(embed=embed))
+    async def channels(bot: Bot, dm_channel: discord.abc.Messageable):
+        lines = []
+        for guild in bot.guilds:
+            guild_state = bot.guild_state_map.get(guild.id)
+            if guild_state is None:
+                continue
+            for id_channel in guild_state['channel_state_map'].copy():
+                channel_state = guild_state['channel_state_map'].get(id_channel)
+                if channel_state is None:
+                    continue
+                track_type = channel_state['type']
+                cmp = 'r2' if track_type == TrackType.MVP else 'r1'
+                active_entries_num = 0
+                for entry_state in channel_state['entry_state_list']:
+                    if entry_state[cmp] > -bot.TABLE_ENTRY_EXPIRATION_MINS:
+                        active_entries_num += 1
+                if active_entries_num > 0:
+                    owner = None
+                    if guild.owner_id is not None:
+                        owner = await CoroutineUtil.run(guild.fetch_member(guild.owner_id))
+                    if owner is None:
+                        continue
+                    channel = ChannelService.get_channel_from_guild(guild, channel_state['id_channel'])
+                    if channel is None:
+                        continue
+                    line = f"{channel} | "
+                    line += f"{track_type.sql_desc} | "
+                    line += f"{guild} | "
+                    line += f"{owner} | "
+                    line += f"{'Yes' if guild_state['custom'] else 'No'} | "
+                    line += f"{'Yes' if guild_state['mobile'] else 'No'} | "
+                    line += f"{active_entries_num}"
+                    lines.append(line)
+        title = "Channels"
+        header = "Channel | Type | Guild | Owner | Custom | Mobile | Entries"
+        await OnMessageEvent.send_msg_paginated(bot, dm_channel, title, header, lines)
 
     @staticmethod
-    def validate(bot: Bot, message: discord.Message) -> bool:
+    async def send_msg_paginated(bot: Bot, dm_channel: discord.abc.Messageable, title: str, header: str, lines: list[str]):
+        if lines == 0:
+            embed = discord.Embed(color=discord.Colour.gold(), title=title)
+            embed.add_field(name=header, value="Empty", inline=False)
+            await CoroutineUtil.run(dm_channel.send(embed=embed))
+            return
+        page_size = 10
+        msg = ""
+        offset = 1
+        count = 0
+        for line in lines:
+            msg += f"{line}\n"
+            count += 1
+            if count == page_size:
+                embed = discord.Embed(color=discord.Colour.gold(), title=title)
+                embed.add_field(name=header, value=msg, inline=False)
+                embed.set_footer(text=f"{offset} to {offset + count - 1} (Total: {len(lines)})")
+                await CoroutineUtil.run(dm_channel.send(embed=embed))
+                msg = ""
+                offset = offset + count
+                count = 0
+        if count > 0:
+            embed = discord.Embed(color=discord.Colour.gold(), title=title)
+            embed.add_field(name=header, value=msg, inline=False)
+            embed.set_footer(text=f"{offset} to {offset + count - 1} (Total: {len(lines)})")
+            await CoroutineUtil.run(dm_channel.send(embed=embed))
+
+    @staticmethod
+    def validate_message(bot: Bot, message: discord.Message) -> bool:
         if not message.content.startswith("!"):
             return False
         if type(message.channel) != discord.DMChannel:
